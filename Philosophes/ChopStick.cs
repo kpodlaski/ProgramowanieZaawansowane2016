e0f79924f4887c048360a531bec70bd71e1a0d09
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2016-11-10 10:16:35 +0100

    Initial v

---
+++ Philosophes/ChopStick.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Philosophes
+{
+    class ChopStick
+    {
+        private volatile bool isUsed = false ;
+
+        public void pickUp()
+        {
+            lock (this) {
+                while(!isUsed){
+                    isUsed = true;
+                }
+            }        
+        }
+
+        public void putDown()
+        {
+            lock (this)
+            {
+                isUsed = false;
+            }
+        }
+
+        public bool canBeUsed () {
+            lock (this)
+            {
+                return isUsed;
+            }
+        }
+
+
+    }
+}
---
+++ Philosophes/Philosoph.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Philosophes
+{
+    class Philosoph
+    {
+        private static Random rand = new Random();
+        public String Name { get;  set;}
+        public int sleepParameter =500;
+        private ChopStick left, right;
+        public void Eat()
+        {
+            Console.WriteLine(Name + " starts Eatind");
+            right.pickUp();
+            left.pickUp();
+            Thread.Sleep(rand.Next() % sleepParameter);
+            left.putDown();
+            right.putDown();
+            Console.WriteLine(Name + " finished Eating");
+        }
+
+        public void Think()
+        {
+            Console.WriteLine(Name + " Thinks");
+            Thread.Sleep(rand.Next() % sleepParameter);
+        }
+
+        public void Live()
+        {
+            while (true)
+            {
+                Think();
+                Eat();
+            }
+        }
+    }
+}
